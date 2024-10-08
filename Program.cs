eda59c63455ca6603a98fb08e1c542c619eed349
Josie Cialdini <jcialdini@my.wctc.edu>
2024-10-08 14:02:01 -0500

    [1.10] option 1

--- Program.cs
+++ Program.cs
@@ -25,6 +25,10 @@ do
   if (choice == "1")
   {
     // Display Mario Characters
+        foreach(var c in marios)
+    {
+      Console.WriteLine(c.Display());
+    }
   }
   else if (choice == "2")
   {
