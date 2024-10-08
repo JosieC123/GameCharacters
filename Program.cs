286b27841cbc8f503c178878432d5bac68bf2d41
Josie Cialdini <jcialdini@my.wctc.edu>
2024-10-08 14:26:28 -0500

    [1.12d] Remove Character

--- Program.cs
+++ Program.cs
@@ -14,83 +14,94 @@ List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(ma
 
 do
 {
-  // display choices to user
-  Console.WriteLine("1) Display Mario Characters");
-  Console.WriteLine("2) Add Mario Character");
-  Console.WriteLine("3) Remove Mario Character");
-  Console.WriteLine("Enter to quit");
+    // display choices to user
+    Console.WriteLine("1) Display Mario Characters");
+    Console.WriteLine("2) Add Mario Character");
+    Console.WriteLine("3) Remove Mario Character");
+    Console.WriteLine("Enter to quit");
 
-  // input selection
-  string? choice = Console.ReadLine();
-  logger.Info("User choice: {Choice}", choice);
+    // input selection
+    string? choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
 
-  if (choice == "1")
-  {
-    // Display Mario Characters
-    foreach(var c in marios)
+    if (choice == "1")
     {
-      Console.WriteLine(c.Display());
+        // Display Mario Characters
+        foreach (var c in marios)
+        {
+            Console.WriteLine(c.Display());
+        }
     }
-  }
-  else if (choice == "2")
-  {
-    // Add Mario Character
-    // Generate unique Id
-    Mario mario = new()
+    else if (choice == "2")
     {
-      Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
-    };
-    
-    InputCharacter(mario);
+        // Add Mario Character
+        // Generate unique Id
+        Mario mario = new()
+        {
+            Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
+        };
 
-    // Add Character
-    marios.Add(mario);
-    File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
-    logger.Info($"Character added: {mario.Name}");
-  }
-  else if (choice == "3")
-  {
-    // Remove Mario Character
-    Console.WriteLine("Enter the Id of the character to remove:");
-    if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
-    {
-      Mario? character = marios.FirstOrDefault(c => c.Id == Id);
-      if (character == null)
-      {
-        logger.Error($"Character Id {Id} not found");
-      } else {
-        logger.Info($"Character Id {Id} found");
-      }
-    } else {
-      logger.Error("Invalid Id");
+        InputCharacter(mario);
+
+        // Add Character
+        marios.Add(mario);
+        File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
+        logger.Info($"Character added: {mario.Name}");
     }
-  }
+    else if (choice == "3")
+    {
+        // Remove Mario Character
+        Console.WriteLine("Enter the Id of the character to remove:");
+        if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
+        {
+            Mario? character = marios.FirstOrDefault(c => c.Id == Id);
+            if (character == null)
+            {
+                logger.Error($"Character Id {Id} not found");
+            }
+            else
+            {
+                marios.Remove(character);
+                // serialize list<marioCharacter> into json file
+                File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
+                logger.Info($"Character Id {Id} removed");
+            }
+        }
+        else
+        {
+            logger.Error("Invalid Id");
+        }
+    }
 } while (true);
 
 logger.Info("Program ended");
 
 static void InputCharacter(Character character)
 {
-  Type type = character.GetType();
-  PropertyInfo[] properties = type.GetProperties();
-  var props = properties.Where(p => p.Name != "Id");
-  foreach (PropertyInfo prop in props)
-  {
-    if (prop.PropertyType == typeof(string))
+    Type type = character.GetType();
+    PropertyInfo[] properties = type.GetProperties();
+    var props = properties.Where(p => p.Name != "Id");
+    foreach (PropertyInfo prop in props)
     {
-      Console.WriteLine($"Enter {prop.Name}:");
-      prop.SetValue(character, Console.ReadLine());
-    } else if (prop.PropertyType == typeof(List<string>)) {
-      List<string> list = [];
-      do {
-        Console.WriteLine($"Enter {prop.Name} or (enter) to quit:");
-        string response = Console.ReadLine()!;
-        if (string.IsNullOrEmpty(response)){
-          break;
+        if (prop.PropertyType == typeof(string))
+        {
+            Console.WriteLine($"Enter {prop.Name}:");
+            prop.SetValue(character, Console.ReadLine());
         }
-        list.Add(response);
-      } while (true);
-      prop.SetValue(character, list);
+        else if (prop.PropertyType == typeof(List<string>))
+        {
+            List<string> list = [];
+            do
+            {
+                Console.WriteLine($"Enter {prop.Name} or (enter) to quit:");
+                string response = Console.ReadLine()!;
+                if (string.IsNullOrEmpty(response))
+                {
+                    break;
+                }
+                list.Add(response);
+            } while (true);
+            prop.SetValue(character, list);
+        }
     }
-  }
 }
