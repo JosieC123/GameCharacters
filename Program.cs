d0eace80589633d2da4d9958b276ae3db8f4214b
Josie Cialdini <jcialdini@my.wctc.edu>
2024-10-08 14:16:51 -0500

    [1.11e] Refactor

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using NLog;
+using System.Reflection;
 using System.Text.Json;
-
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -14,73 +14,74 @@ List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(ma
 
 do
 {
-    // display choices to user
-    Console.WriteLine("1) Display Mario Characters");
-    Console.WriteLine("2) Add Mario Character");
-    Console.WriteLine("3) Remove Mario Character");
-    Console.WriteLine("Enter to quit");
-    // input selection
-    string? choice = Console.ReadLine();
-    logger.Info("User choice: {Choice}", choice);
+  // display choices to user
+  Console.WriteLine("1) Display Mario Characters");
+  Console.WriteLine("2) Add Mario Character");
+  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Enter to quit");
 
-    if (choice == "1")
+  // input selection
+  string? choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Display Mario Characters
+    foreach(var c in marios)
     {
-
-        // Display Mario Characters
-        foreach (var c in marios)
-        {
-            Console.WriteLine(c.Display());
-        }
-
+      Console.WriteLine(c.Display());
     }
-    else if (choice == "2")
+  }
+  else if (choice == "2")
+  {
+    // Add Mario Character
+    // Generate unique Id
+    Mario mario = new()
     {
+      Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
+    };
+    
+    InputCharacter(mario);
 
-        // Add Mario Character
-        // Generate unique Id
-        Mario mario = new()
-        {
-            Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
-        };
-
-        // Input Name, Description
-        Console.WriteLine("Enter Name:");
-        mario.Name = Console.ReadLine();
-        Console.WriteLine("Enter Description:");
-        mario.Description = Console.ReadLine();
-
-        // Input Alias
-        List<string> list = [];
-        do
-        {
-            Console.WriteLine($"Enter Alias or (enter) to quit:");
-            string response = Console.ReadLine()!;
-            if (string.IsNullOrEmpty(response))
-            {
-                break;
-            }
-            list.Add(response);
-        } while (true);
-        mario.Alias = list;
-        // Add Character
-        marios.Add(mario);
-        File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
-        logger.Info($"Character added: {mario.Name}");
-
-    }
-    else if (choice == "3")
-    {
-        // Remove Mario Character
-    }
-    else if (string.IsNullOrEmpty(choice))
-    {
-        break;
-    }
-    else
-    {
-        logger.Info("Invalid choice");
-    }
+    // Add Character
+    marios.Add(mario);
+    File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
+    logger.Info($"Character added: {mario.Name}");
+  }
+  else if (choice == "3")
+  {
+    // Remove Mario Character
+  } else if (string.IsNullOrEmpty(choice)) {
+    break;
+  } else {
+    logger.Info("Invalid choice");
+  }
 } while (true);
 
-
 logger.Info("Program ended");
+
+static void InputCharacter(Character character)
+{
+  Type type = character.GetType();
+  PropertyInfo[] properties = type.GetProperties();
+  var props = properties.Where(p => p.Name != "Id");
+  foreach (PropertyInfo prop in props)
+  {
+    if (prop.PropertyType == typeof(string))
+    {
+      Console.WriteLine($"Enter {prop.Name}:");
+      prop.SetValue(character, Console.ReadLine());
+    } else if (prop.PropertyType == typeof(List<string>)) {
+      List<string> list = [];
+      do {
+        Console.WriteLine($"Enter {prop.Name} or (enter) to quit:");
+        string response = Console.ReadLine()!;
+        if (string.IsNullOrEmpty(response)){
+          break;
+        }
+        list.Add(response);
+      } while (true);
+      prop.SetValue(character, list);
+    }
+  }
+}
